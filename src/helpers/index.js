037c7807befde7f3516f5097f1dc896b0aab0fd4
AB-programmer8 <109824061+AB-programmer8@users.noreply.github.com>
2026-02-23 22:49:22 +0000

    Stop dead players from talking during a game

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,17 @@
 export { default as loadEvents } from "./loadEvents";
 export { default as loadFiles } from "./loadFiles";
+import { alivePlayers, currentPhase } from "./helpers/gameState.js";
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.channel.name !== "mafia-game") return;
+
+  if (currentPhase === "PRE_GAME" || currentPhase === "ENDED") return;
+
+  if (!alivePlayers.has(message.author.id)) {
+    await message.delete().catch(() => {});
+    await message.author.send("☠️ Dead men tell no tales...");
+  }
+});
+
