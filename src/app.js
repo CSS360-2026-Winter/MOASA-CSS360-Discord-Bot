81163f1f943bf15a0a41b8546ef9f16f9c1c46ac
AB-programmer8 <109824061+AB-programmer8@users.noreply.github.com>
2026-02-24 20:56:33 +0000

    Fix error with dead users not being able to message in mafia-channel

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits, Partials, Collection } from "discord.js";
 import { loadEvents } from "./helpers";
 import path from "path";
 import { loadCommands } from "./helpers/loadCommands";
+import { alivePlayers, currentPhase } from "./helpers/gameState";
 
 
 
@@ -14,6 +15,20 @@ const { User, Message, GuildMember, ThreadMember } = Partials;
 const client = new Client({
   intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
   partials: [User, Message, GuildMember, ThreadMember],
+});
+
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.channel.name !== "mafia-game") return;
+
+  if (currentPhase === "PRE_GAME" || currentPhase === "ENDED") return;
+
+  if (!alivePlayers.has(message.author.id)) {
+    await message.delete().catch(() => {});
+    await message.author.send("☠️ You are a dead civilian.. Please refrain from putting messages in the mafia channel... Dead men tell no tales...");
+  }
 });
 
 client.events = new Collection();
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,17 +1,4 @@
 export { default as loadEvents } from "./loadEvents";
 export { default as loadFiles } from "./loadFiles";
-import { alivePlayers, currentPhase } from "./helpers/gameState.js";
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-
-  if (message.channel.name !== "mafia-game") return;
-
-  if (currentPhase === "PRE_GAME" || currentPhase === "ENDED") return;
-
-  if (!alivePlayers.has(message.author.id)) {
-    await message.delete().catch(() => {});
-    await message.author.send("☠️ Dead men tell no tales...");
-  }
-});
 
